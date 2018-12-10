7a953ec27bb67adcdbf831d9d035bd1d1ec31484
Fozail Ahmad <25251550+FozAhm@users.noreply.github.com>
2018-12-10 10:55:53 -0500

    Update Script
    Allow Script to Run Google Authenticator

--- mfs.sh
+++ mfs.sh
@@ -2,7 +2,7 @@
 clear
 sudo apt-get update
 sudo apt-get install libpam-google-authenticator -y
-google-authenticator -t -d -f -q -r 3 -R 30 -w 3
+/usr/bin/google-authenticator -t -d -f -q -r 3 -R 30 -w 3
 sudo /bin/su -c "echo 'auth required pam_google_authenticator.so nullok' >> /etc/pam.d/sshd"
 sudo sed -i 's/@include common-auth/# @include common-auth/' /etc/pam.d/sshd
 sudo sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
